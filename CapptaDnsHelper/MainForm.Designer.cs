ec0496e25bccad4d2766b5192eb03898488726a4
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2017-06-14 16:48:12 -0300

    Initial Version

---
+++ CapptaDnsHelper/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CapptaDnsHelper
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.googleDnsButton = new System.Windows.Forms.Button();
+			this.openDnsButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// googleDnsButton
+			// 
+			this.googleDnsButton.Location = new System.Drawing.Point(12, 12);
+			this.googleDnsButton.Name = "googleDnsButton";
+			this.googleDnsButton.Size = new System.Drawing.Size(260, 48);
+			this.googleDnsButton.TabIndex = 0;
+			this.googleDnsButton.Text = "Configurar para Google\r\n8.8.8.8 e 8.8.4.4";
+			this.googleDnsButton.UseVisualStyleBackColor = true;
+			this.googleDnsButton.Click += new System.EventHandler(this.googleDnsButton_Click);
+			// 
+			// openDnsButton
+			// 
+			this.openDnsButton.Location = new System.Drawing.Point(12, 66);
+			this.openDnsButton.Name = "openDnsButton";
+			this.openDnsButton.Size = new System.Drawing.Size(260, 48);
+			this.openDnsButton.TabIndex = 1;
+			this.openDnsButton.Text = "Configurar para OpenDNS\r\n208.67.222.222 e 208.67.220.220";
+			this.openDnsButton.UseVisualStyleBackColor = true;
+			this.openDnsButton.Click += new System.EventHandler(this.openDnsButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 131);
+			this.Controls.Add(this.openDnsButton);
+			this.Controls.Add(this.googleDnsButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "MainForm";
+			this.Text = "Utilitario de DNS da Cappta";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button googleDnsButton;
+		private System.Windows.Forms.Button openDnsButton;
+	}
+}
+
---
+++ CapptaDnsHelper/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Windows.Forms;
+
+namespace CapptaDnsHelper
+{
+	public partial class MainForm : Form
+	{
+		private static readonly string[] googleDns = new string[] { "8.8.8.8", "8.8.4.4" };
+		private static readonly string[] openDns = new string[] { "208.67.222.222", "208.67.220.220" };
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private IEnumerable<string> SetDns(string[] dns)
+		{
+			var networkAdapterConfiguration = new ManagementClass("Win32_NetworkAdapterConfiguration");
+			var networkAdapterConfigurationInstances = networkAdapterConfiguration.GetInstances();
+
+			foreach (var managementBaseObject in networkAdapterConfigurationInstances)
+			{
+				var networkAdapter = managementBaseObject as ManagementObject;
+
+				if ((bool)networkAdapter["IPEnabled"] == false) { continue; }
+
+				try
+				{
+					var newDNS = networkAdapter.GetMethodParameters("SetDNSServerSearchOrder");
+					newDNS["DNSServerSearchOrder"] = dns;
+					var setDNS = networkAdapter.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+					var result = (uint)setDNS["returnValue"];
+					if (result != 0) { continue; }
+				}
+				catch { continue; }
+
+				yield return (string)networkAdapter["Caption"];
+			}
+		}
+
+		private void SetDnsAndNotify(string[] dns)
+		{
+			var updatedAdapters = this.SetDns(dns).ToArray();
+			switch (updatedAdapters.Length)
+			{
+				case 0: MessageBox.Show(this, "Não conseguimos alterar o DNS", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return;
+				case 1: MessageBox.Show(this, $"Alteramos o DNS do adaptador de rede \"{updatedAdapters.First()}\" para {string.Join(" e ", dns)}", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information); return;
+				default: MessageBox.Show(this, $"Alteramos o DNS dos adaptador de rede \"{string.Join("\", \"", updatedAdapters)}\" para {string.Join(" e ", dns)}", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information); return;
+			}
+		}
+
+		private void googleDnsButton_Click(object sender, EventArgs e)
+		{
+			this.SetDnsAndNotify(googleDns);
+		}
+
+		private void openDnsButton_Click(object sender, EventArgs e)
+		{
+			this.SetDnsAndNotify(openDns);
+		}
+	}
+}
